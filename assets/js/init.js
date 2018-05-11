d369492a78f975b68720d229a204309e199f4c6e
judasane <judasane@gmail.com>
2018-05-11 09:08:35 -0500

    first prod

---
+++ assets/js/init.js
@@ -0,0 +1,7 @@
+
+/* var sidel = document.querySelector('.sidenav');
+var instance = new M.Sidenav(sidel); */
+
+
+/* var tabel = document.querySelector('.tabs');
+var instance = new M.Tabs(tabel); */
